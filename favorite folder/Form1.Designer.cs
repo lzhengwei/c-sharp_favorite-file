56c347e9c3b24dfaaafc0b6042446916a0e13442
zhengwei <b840615@gmail.com>
2018-02-23 15:46:13 +0800

    First upload

---
+++ favorite folder/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace favorite_folder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView_main = new System.Windows.Forms.ListView();
+            this.button_exit = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.button_exit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView_main
+            // 
+            this.listView_main.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.listView_main.BackgroundImage = global::favorite_folder.Properties.Resources._11843973_1181665195183723_183390284_n;
+            this.listView_main.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView_main.Location = new System.Drawing.Point(50, 76);
+            this.listView_main.Name = "listView_main";
+            this.listView_main.Size = new System.Drawing.Size(678, 354);
+            this.listView_main.TabIndex = 0;
+            this.listView_main.UseCompatibleStateImageBehavior = false;
+            this.listView_main.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView_main_MouseDoubleClick);
+            this.listView_main.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listView_main_MouseDown);
+            // 
+            // button_exit
+            // 
+            this.button_exit.BackColor = System.Drawing.Color.Transparent;
+            this.button_exit.Location = new System.Drawing.Point(357, 3);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(64, 32);
+            this.button_exit.TabIndex = 1;
+            this.button_exit.TabStop = false;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::favorite_folder.Properties.Resources.tablet;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(779, 512);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.listView_main);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.form_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.form_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.form_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.button_exit)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_main;
+        private System.Windows.Forms.PictureBox button_exit;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ favorite folder/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace favorite_folder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<String> filelist;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ImageList iconList = new ImageList();
+            iconList.ImageSize = new Size(60, 60);
+            iconList.Images.Add(
+                Image.FromFile(Application.StartupPath + @"\..\..\Resources\Excel.png"));
+            iconList.Images.Add(
+                Image.FromFile(Application.StartupPath + @"\..\..\Resources\word.png"));
+            iconList.Images.Add(
+                Image.FromFile(Application.StartupPath + @"\..\..\Resources\ppt.png"));
+            iconList.Images.Add(
+                Image.FromFile(Application.StartupPath + @"\..\..\Resources\pdf.png"));
+            iconList.Images.Add(
+                Image.FromFile(Application.StartupPath + @"\..\..\Resources\file.png"));
+            iconList.Images.Add(
+                Image.FromFile(Application.StartupPath + @"\..\..\Resources\Folder.png"));
+            listView_main.Items.Clear();
+            listView_main.LargeImageList = iconList;
+
+            listView_main.Columns.Add("File", 50);
+            listView_main.View = View.LargeIcon;
+
+            listView_main.Font = new Font(FontFamily.GenericSansSerif,
+            14.0F, FontStyle.Bold);
+
+            filelist = new List<string>();
+            InitialList();
+        }
+        //================= form move ==================================================
+        bool formMove = false;//窗体是否移动
+        Point formPoint;//记录窗体的位置
+
+        private void form_MouseDown(object sender, MouseEventArgs e)
+        {
+            formPoint = new Point();
+            int xOffset;
+            int yOffset;
+            if (e.Button == MouseButtons.Left)
+            {
+                xOffset = -e.X - SystemInformation.FrameBorderSize.Width;
+                yOffset = -e.Y - SystemInformation.CaptionHeight - SystemInformation.FrameBorderSize.Height;
+                formPoint = new Point(xOffset, yOffset);
+                formMove = true;//开始移动
+            }
+        }
+
+        private void form_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (formMove == true)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(formPoint.X, formPoint.Y);
+                Location = mousePos;
+            }
+        }
+
+        private void form_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)//按下的是鼠标左键
+            {
+                formMove = false;//停止移动
+            }
+        }
+
+        private void button_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void listView_main_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Items.Clear();
+                contextMenuStrip1.Items.Add("Excute");
+                contextMenuStrip1.Items.Add("Add File");
+                contextMenuStrip1.Items.Add("Add Folder");
+                contextMenuStrip1.Items.Add("Remove");
+
+                contextMenuStrip1.Show(MousePosition);
+            }
+        }
+
+        private void contextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Text)
+            {
+                case "Excute":
+                    Process.Start(filelist[listView_main.SelectedItems[0].Index]);                
+                    break;
+                case "Add File":
+                    Addnewfile();
+                    break;
+                case "Add Folder":
+                    Addnewfolder();
+                    break;
+                case "Delete":
+                    Removefile();
+                    break;
+            }
+        }
+
+        private void Addnewfolder()
+        {
+            folderBrowserDialog1.Tag = "選擇資料夾";
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filelist.Add(folderBrowserDialog1.SelectedPath);
+            }
+            refreshfilelist();
+        }
+        private void InitialList()
+        {
+             String readfilelist=Properties.Settings.Default.Filename_string;
+             String[] filestring = readfilelist.Split('\n');
+             foreach (String filename in filestring)
+             {
+                 if (filename.Length>5)
+                 {
+                    filelist.Add(filename);
+                 }
+             }
+             refreshfilelist();
+        }
+        private void Addnewfile()
+        {
+            openFileDialog1.Tag = "選擇檔案";
+            
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filelist.Add(openFileDialog1.FileName);
+            }
+            refreshfilelist();
+        }
+        private void Removefile()
+        {
+            int index = listView_main.SelectedItems[0].Index;
+            if(index>=0)
+                filelist.RemoveAt(index);
+            refreshfilelist();
+        }
+        private void refreshfilelist()
+        {
+            listView_main.Items.Clear();
+            foreach (String filename in filelist)
+            {
+                FileInfo file=new FileInfo(filename);
+                switch (file.Extension)
+                {
+                    case ".xlsx":
+                    case ".xls":
+                        listView_main.Items.Add(System.IO.Path.GetFileName(filename), 0);
+                        break;
+                    case ".doc":
+                    case ".docx":
+                        listView_main.Items.Add(System.IO.Path.GetFileName(filename), 1);
+                        break;
+                    case ".ppt":
+                    case ".pptx":
+                        listView_main.Items.Add(System.IO.Path.GetFileName(filename), 2);
+                        break;
+                    case ".pdf":
+                        listView_main.Items.Add(System.IO.Path.GetFileName(filename), 3);
+                        break;
+                    case "":
+                        listView_main.Items.Add(System.IO.Path.GetFileName(filename), 5);
+                        break;
+                    default:
+                        listView_main.Items.Add(System.IO.Path.GetFileName(filename), 4);
+                        break;
+                }
+                
+            }
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            String savefilelist="";
+            foreach(String filename in filelist)
+            {
+                savefilelist += filename + "\n";
+            }
+            Properties.Settings.Default.Filename_string = savefilelist;
+            Properties.Settings.Default.Save();
+        }
+
+        private void listView_main_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Point localPoint = listView_main.PointToClient(MousePosition);
+            var item = listView_main.GetItemAt(localPoint.X, localPoint.Y);
+            int index = item.Index;
+            if (index >= 0)
+            {
+                Process.Start(filelist[listView_main.SelectedItems[0].Index]);       
+            }
+        }
+        
+    }
+}
